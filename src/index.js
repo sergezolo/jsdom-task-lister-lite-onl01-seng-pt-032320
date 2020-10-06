35c7934534f41344f62017ca64f44619297f061f
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-10-05 19:13:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const todoList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const userInputField = event.target.querySelector("#new-task-description");
+    const userInput = userInputField.value;
+    const liTag = document.createElement("li");
+    liTag.innerText = userInput;
+    todoList.appendChild(liTag);
+    userInputField.value = "";
+  });
+
 });
